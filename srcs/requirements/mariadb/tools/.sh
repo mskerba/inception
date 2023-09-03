a6cd2ef0298092081335343cb770d75d9d3e662e
mskerba <mskerba@mskerba>
2023-09-03 16:56:31 +0100

    all is done

--- srcs/requirements/mariadb/tools/.sh
+++ srcs/requirements/mariadb/tools/.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-
 service mysql start
 mysql -e "CREATE DATABASE admin"
 mysql -e "CREATE USER 'monaim'@'%' IDENTIFIED BY 'password'"
-# mysql -e "GRANTS FOR 'monaim'@'localhost'"
-# mysql -e "GRANT USAGE ON *.* TO 'monaim'@'localhost'"
-# mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE ON `admin`.* TO 'monaim'@'localhost'"
 mysql -e "GRANT ALL PRIVILEGES ON admin.* TO 'monaim'@'%'"
 service mysql stop
 mysqld_safe
--- srcs/requirements/wordpress/tools/.sh
+++ srcs/requirements/wordpress/tools/.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
 sleep 5
-wp core install  --url=https://localhost:8080/ --title=mskerba --admin_user=mskerba --admin_email=monaimskerba@gmail.com --allow-root --path=/shared-dat/wordpress/
+wp core install  --url=https://localhost:8080/ --title=mskerba --admin_user=mskerba --admin_email=monaimskerba@gmail.com --allow-root --path=/shard_data/wordpress/
 
 php-fpm7.3 -F
